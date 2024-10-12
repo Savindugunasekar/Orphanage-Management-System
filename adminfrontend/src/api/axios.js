8f1cf93ee4d3661d160f61f3cbd2ce6ad98d4ef3
malinduGamage <malindug.21@cse.mrt.ac.lk>
2024-10-12 11:51:58 +0530

    base URL FIX

--- adminfrontend/src/api/axios.js
+++ adminfrontend/src/api/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:4000'
+const BASE_URL = 'http://172.17.0.2:4000'
 
 export default axios.create({
-    baseURL:BASE_URL,
-    withCredentials:true
+    baseURL: BASE_URL,
+    withCredentials: true
 })
 
 export const axiosPrivate = axios.create({
