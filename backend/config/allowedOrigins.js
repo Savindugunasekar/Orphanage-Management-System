e4f1e273f675b70a8359786a76832ddf56029344
malinduGamage <malindug.21@cse.mrt.ac.lk>
2024-10-12 11:53:26 +0530

    url add

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3002',
     'http://localhost:3000',
-    'http://172.17.0.2:3000'
+    'http://16.16.186.73:3000'
 ];
 
 module.exports = allowedOrigins;
