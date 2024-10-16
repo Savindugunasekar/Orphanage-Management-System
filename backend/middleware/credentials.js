48bc0b814941af89ec666510611cef234946a713
malinduGamage <malindug.21@cse.mrt.ac.lk>
2024-10-16 14:29:52 +0530

    credential condition fix

--- backend/middleware/credentials.js
+++ backend/middleware/credentials.js
@@ -3,9 +3,9 @@ const allowedOrigins = require('../config/allowedOrigins')
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
 
-
-    res.header('Access-Control-Allow-Credentials', true)
-
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Credentials', true)
+    }
 
     next()
 }
